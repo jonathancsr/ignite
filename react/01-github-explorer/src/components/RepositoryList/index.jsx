b32d39637e28eb3bd67066250053189d0e4f2cc1
jonathan.candido <jonathan.candido@toroinvestimentos.com.br>
2021-11-02 23:11:04 -0300

    chore: add github api fetch

--- react/01-github-explorer/src/components/RepositoryList/index.jsx
+++ react/01-github-explorer/src/components/RepositoryList/index.jsx
@@ -1,26 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { RepositoryItem } from "../RepositoryItem"
 
 import './style.scss'
 
-const repository = {
-    name:"Unform 2",
-    description:"description" ,
-    link:"https://github.com/jonathancsr/recuperacao-da-informacao",
-}
-
-
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+            const response = fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    },[]);
+    
+
     return (
         <section className='repository-list'>
             <h1>Repository list</h1>
             <ul>
-               <RepositoryItem 
-                    repository={repository}
-                />
-               <RepositoryItem/>
-               <RepositoryItem/>
-               <RepositoryItem/>
+                {/* {repositories ?? repositories.map(repository => <RepositoryItem repository={repository}/>)} */}
             </ul>
         </section>
     )
