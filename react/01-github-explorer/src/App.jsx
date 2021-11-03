ce270cd4a4f046d5f0ad753bb21b4b163bc44fe1
jonathan.candido <jonathan.candido@toroinvestimentos.com.br>
2021-11-02 21:30:23 -0300

    feat: add css import

--- react/01-github-explorer/src/App.jsx
+++ react/01-github-explorer/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 
+import './styles/global.css'
+
+
 export function App() {
     return <h1>Hello World</h1>
 }
--- react/01-github-explorer/webpack.config.js
+++ react/01-github-explorer/webpack.config.js
@@ -23,15 +23,21 @@ module.exports = {
         template: path.resolve(__dirname, 'public', 'index.html'),
     })],
     module:{
-        rules:[{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+        rules:[
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
                 }
+            },{
+                test: /\.css?$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
             }
-        }],
+        ],
     }
 };
