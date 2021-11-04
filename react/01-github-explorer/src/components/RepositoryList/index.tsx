36d915a4ffd86af2c31e61dc6ec8ed613bc5d3e1
jonathan.candido <jonathan.candido@toroinvestimentos.com.br>
2021-11-03 23:58:25 -0300

    chore: change components to typescript

--- react/01-github-explorer/src/components/RepositoryList/index.tsx
+++ react/01-github-explorer/src/components/RepositoryList/index.tsx
@@ -3,8 +3,15 @@ import { RepositoryItem } from "../RepositoryItem"
 
 import './style.scss'
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+
 export function RepositoryList(){
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
             const response = fetch('https://api.github.com/orgs/rocketseat/repos')
